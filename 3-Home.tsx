169b70294ab5f0cdf8eac9c9bcae149dce485257
kipyek <deniskipyegon88@gmail.com>
2023-03-03 13:19:24 +0300

    Navigation fix

--- 3-Home.tsx
+++ 3-Home.tsx
@@ -2,18 +2,18 @@ import React from "react";
 import { Text, TouchableHighlight, View, Image, StyleSheet } from "react-native";
 import setting from "./3-Setting";
 
-export default function Home(props:any) {
-    return(
+export default function Home(props: any) {
+    return (
         <View style={styles.container}>
             <Image
-            source={require('../project-pics/photos/quiz.webp')}
-            style={styles.image}
+                source={require('../project-pics/photos/quiz.webp')}
+                style={styles.image}
             />
             <View style={styles.main}>
-            <Text style={styles.txt}>The Quiz App</Text>
-            <TouchableHighlight style={styles.button} onPress={()=>props.navigation.navigate('setting')}>
-                <Text style={styles.txt2}>Start</Text>
-            </TouchableHighlight>
+                <Text style={styles.txt}>The Quiz App</Text>
+                <TouchableHighlight style={styles.button} onPress={() => props.navigation.navigate('Setting')}>
+                    <Text style={styles.txt2}>Start</Text>
+                </TouchableHighlight>
             </View>
         </View>
     )
@@ -21,41 +21,41 @@ export default function Home(props:any) {
 
 const styles = StyleSheet.create({
 
-    container :{
-        flex:1
+    container: {
+        flex: 1
     },
     image: {
-        width:'70%',
-        height:'25%',
-        marginHorizontal:50,
-        marginTop:100,
+        width: '70%',
+        height: '25%',
+        marginHorizontal: 50,
+        marginTop: 100,
     },
-    txt :{
-        color:'black',
-        fontSize:24,
-        textAlign:'center',
-        paddingBottom:10
-        
+    txt: {
+        color: 'black',
+        fontSize: 24,
+        textAlign: 'center',
+        paddingBottom: 10
+
     },
-    button :{
-        backgroundColor:'lightblue',
-        width:'55%',
-        shadowColor:'black',
+    button: {
+        backgroundColor: 'lightblue',
+        width: '55%',
+        shadowColor: 'black',
         shadowOffset: {
-            width:2,
-            height:3,
+            width: 2,
+            height: 3,
         },
-        shadowOpacity:0.25,
-        shadowRadius:4,
-        elevation:5,
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
     },
-    main :{
-        alignItems:'center',
-        marginTop:100
+    main: {
+        alignItems: 'center',
+        marginTop: 100
     },
-    txt2 :{
-        color:'black',
-        fontSize:24,
-        textAlign:'center'
+    txt2: {
+        color: 'black',
+        fontSize: 24,
+        textAlign: 'center'
     }
 })
--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Text, TouchableHighlight, View,Button } from "react-native";
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { Text, TouchableHighlight, View, Button } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Project/3-Home";
 import Account from "./Project/3-account";
-import setting from "./Project/3-Setting"
+import Setting from "./Project/3-Setting"
 
 
 
@@ -13,32 +13,32 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
 
-const btnAction = () => {
-   
-       
-}
+    const btnAction = () => {
 
-    return(
+
+    }
+
+    return (
         <NavigationContainer>
             <Stack.Navigator>
                 <Stack.Screen name='Home' component={Home}
-                options={{
-                    headerLeft:()=><Button  title="Y A"/>,
-                    headerRight:()=><Button onPress={btnAction} title="setting" />,
-                    headerTitleAlign:"center"
-                }}
+                    options={{
+                        headerLeft: () => <Button title="Y A" />,
+                        headerRight: () => <Button onPress={btnAction} title="setting" />,
+                        headerTitleAlign: "center"
+                    }}
                 />
 
-                <Stack.Screen name="Details" component={Account} 
-                options={{
-                    headerTitleAlign:'center',
-                    headerStyle:{
-                        backgroundColor:'skyblue',
-                    }
-                }}
+                <Stack.Screen name="Details" component={Account}
+                    options={{
+                        headerTitleAlign: 'center',
+                        headerStyle: {
+                            backgroundColor: 'skyblue',
+                        }
+                    }}
                 />
 
-                <Stack.Screen name="setting" component={setting}/>
+                <Stack.Screen name="Setting" component={Setting} />
 
 
             </Stack.Navigator>
